89071f6774bab2454e2c716ed035cef12f2438da
0chil <0@chll.it>
2022-07-05 01:34:37 +0900

    feat: 시도 횟수 입력

--- src/main/java/racingcar/view/UserView.java
+++ src/main/java/racingcar/view/UserView.java
@@ -5,10 +5,17 @@ import camp.nextstep.edu.missionutils.Console;
 public class UserView {
 
 	private static final String PROMPT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String PROMPT_TRIAL_COUNT = "시도할 회수는 몇회인가요?";
 
 	public static String readCarName() {
 		System.out.println(PROMPT_CAR_NAME);
 		String input = Console.readLine();
 		return input;
+	}
+
+	public static Integer readTrialCount() {
+		System.out.println(PROMPT_TRIAL_COUNT);
+		String input = Console.readLine();
+		return Integer.parseInt(input);
 	}
 }
