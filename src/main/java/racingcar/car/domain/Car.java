d9f4493d5b97e722acbb875534aa850135250f84
0chil <0@chll.it>
2022-07-14 20:51:20 +0900

    chore: 학습용 주석 삭제

--- src/main/java/racingcar/car/domain/Car.java
+++ src/main/java/racingcar/car/domain/Car.java
@@ -24,19 +24,6 @@ public class Car implements Comparable<Car> {
 		return name;
 	}
 
-	/**
-	 * 학습용 주석
-	 * toString()은 출력 로직을 심는 용도보다는 디버깅 용도로 사용하는 것이 좋겠다.
-	 * 1. 출력 로직을 심게 되면 아무리 잘 하려고 해도 클래스가 출력과 연관된다.
-	 * 2. 그렇게 되면 출력 변경을 위해서 해당 클래스를 바꾸어야 한다.
-	 * -> Layer 분리 실패
-	 * *
-	 * 의문사항
-	 * 그렇다면 디버깅 용도로서의 toString()에서 앞, 중간, 뒤의 String들은 상수화 해야 할까?
-	 * 안 하는게 더 잘 보이기도 하는 것 같다.
-	 * 그렇다면 다시 String.format()을 도입?
-	 */
-
 	@Override
 	public String toString() {
 		return "Car(name=" + name + ", position=" + position + ")";
